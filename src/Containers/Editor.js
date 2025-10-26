6fe86f28a45c90711979596313325a9e676c7fcb
nishi3012 <kadamnishita12@gmail.com>
2025-10-26 21:02:17 +0530

    Trigger new build for environment variables

--- src/Containers/Editor.js
+++ src/Containers/Editor.js
@@ -226,6 +226,187 @@
 
 // export default Editor;
 
+// import React, { Component } from "react";
+// import EditorComponent from "../Components/Editor/EditorComponent";
+// import axios from "axios";
+// import ReconnectingWebSocket from "reconnecting-websocket";
+// import shareDB from "sharedb/lib/client";
+// import StringBinding from "../EditorBinding/StringBinding";
+// import Loader from "../Components/Loader/Loading";
+// import { notification } from "antd";
+
+// // Use environment variables from .env files
+// const serverURL = process.env.REACT_APP_SERVER_URL;
+// const websocketURL = process.env.REACT_APP_WEB_SOCKET_URL;
+
+// class Editor extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       code: "",
+//       input: "",
+//       output: "",
+//       lang: "cpp",
+//       editor: null,
+//       monaco: null,
+//       binding: null,
+//       videoChat: false,
+//       runCodeDisabled: false,
+//       isLoading: true,
+//     };
+//   }
+
+//   componentDidMount() {
+//     const id = this.props.match.params.id;
+//     axios
+//       .post(serverURL, { id: id })
+//       .then((res) => {
+//         const rws = new ReconnectingWebSocket(websocketURL + "/bar");
+//         const connection = new shareDB.Connection(rws);
+//         const doc = connection.get("examples", id);
+
+//         doc.subscribe((err) => {
+//           if (err) throw err;
+//           const presence = connection.getPresence("examples");
+//           presence.subscribe((err) => {
+//             if (err) throw err;
+//           });
+//           let localPresence = presence.create();
+
+//           let binding = new StringBinding(this, doc, ["content"], localPresence);
+//           this.setState({ binding, isLoading: false }, () =>
+//             console.log("binding set")
+//           );
+//           binding.setup(this);
+
+//           presence.on("receive", (id, range) => {
+//             if (!range) return;
+//             let isPos =
+//               range.startLineNumber === range.endLineNumber &&
+//               range.startColumn === range.endColumn;
+//             binding.decorations = this.state.editor.deltaDecorations(
+//               binding.decorations,
+//               [
+//                 {
+//                   range: new this.state.monaco.Range(
+//                     range.startLineNumber,
+//                     range.startColumn,
+//                     range.endLineNumber,
+//                     range.endColumn
+//                   ),
+//                   options: {
+//                     className: isPos ? "cursor-position" : "cursor-selection",
+//                   },
+//                 },
+//               ]
+//             );
+//             binding.range = range;
+//           });
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         notification.error({ message: err.toString() });
+//       });
+//   }
+
+//   editorDidMount = (editor, monaco) => {
+//     editor.focus();
+//     editor.getModel().pushEOL(0);
+
+//     let setup = true;
+//     editor.onDidChangeCursorSelection((e) => {
+//       if (setup) {
+//         let pos = editor.getPosition();
+//         editor.setSelection(
+//           new monaco.Range(pos.lineNumber, pos.column, pos.lineNumber, pos.column)
+//         );
+//         setup = false;
+//         return;
+//       }
+//       if (this.state.binding.localPresence) {
+//         this.state.binding.localPresence.submit(e.selection, (err) => {
+//           if (err) throw err;
+//         });
+//       }
+//     });
+
+//     this.setState({ editor, monaco });
+//   };
+
+//   editorOnChange = (newValue, e) => {
+//     this.state.binding._inputListener(newValue, e);
+//     this.setState({ code: newValue });
+//   };
+
+//   handleRun = () => {
+//     this.setState({ runCodeDisabled: true });
+//     const code = this.state.editor.getValue();
+
+//     axios
+//       .post(`${serverURL}/code/run`, {
+//         code: code,
+//         input: this.state.input,
+//         id: this.props.match.params.id,
+//         lang: this.state.editor.getModel().getLanguageIdentifier().language,
+//       })
+//       .then((response) => {
+//         this.state.binding._inoutListener(this.state.output, response.data, "output");
+//         this.setState({ output: response.data, runCodeDisabled: false });
+//       })
+//       .catch((err) => {
+//         if (!err.response) {
+//           notification.error({ message: err.toString() });
+//           this.setState({ runCodeDisabled: false });
+//         } else if (err.response.status === 400) {
+//           this.state.binding._inoutListener(this.state.output, err.response.data, "output");
+//           this.setState({ output: err.response.data, runCodeDisabled: false });
+//         }
+//       });
+//   };
+
+//   handleInput = (e) => {
+//     this.state.binding._inoutListener(this.state.input, e.target.value, "input");
+//     this.setState({ input: e.target.value });
+//   };
+
+//   handleLang = (value) => {
+//     this.state.binding._inoutListener(this.state.lang, value, "lang");
+//     this.setState({ lang: value });
+//   };
+
+//   handleVideoChat = () => {
+//     this.setState({ videoChat: !this.state.videoChat });
+//   };
+
+//   render() {
+//     const { videoChat, lang, code, input, output, runCodeDisabled, isLoading } = this.state;
+//     return (
+//       <React.Fragment>
+//         {isLoading && <Loader />}
+//         <EditorComponent
+//           videoChat={videoChat}
+//           lang={lang}
+//           code={code}
+//           input={input}
+//           output={output}
+//           runCodeDisabled={runCodeDisabled}
+//           readOnly={isLoading}
+//           handleVideoChat={this.handleVideoChat}
+//           editorDidMount={this.editorDidMount}
+//           editorOnChange={this.editorOnChange}
+//           handleLang={this.handleLang}
+//           handleRun={this.handleRun}
+//           handleInput={this.handleInput}
+//         />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default Editor;
+
+
 import React, { Component } from "react";
 import EditorComponent from "../Components/Editor/EditorComponent";
 import axios from "axios";
@@ -235,9 +416,9 @@ import StringBinding from "../EditorBinding/StringBinding";
 import Loader from "../Components/Loader/Loading";
 import { notification } from "antd";
 
-// Use environment variables from .env files
-const serverURL = process.env.REACT_APP_SERVER_URL;
-const websocketURL = process.env.REACT_APP_WEB_SOCKET_URL;
+// Use environment variables from Netlify build
+const serverURL = process.env.REACT_APP_SERVER_URL; // e.g., "https://backend-f57ybo300-kadamnishita12-8917s-projects.vercel.app"
+const websocketURL = process.env.REACT_APP_WEB_SOCKET_URL; // e.g., "wss://backend-f57ybo300-kadamnishita12-8917s-projects.vercel.app"
 
 class Editor extends Component {
   constructor(props) {
@@ -257,22 +438,29 @@ class Editor extends Component {
   }
 
   componentDidMount() {
+    console.log("Server URL:", serverURL);
+    console.log("WebSocket URL:", websocketURL);
+
     const id = this.props.match.params.id;
+
+    // Fetch initial document
     axios
-      .post(serverURL, { id: id })
+      .post(`${serverURL}/code/run`, { id })
       .then((res) => {
-        const rws = new ReconnectingWebSocket(websocketURL + "/bar");
+        // Setup WebSocket
+        const rws = new ReconnectingWebSocket(`${websocketURL}/bar`);
         const connection = new shareDB.Connection(rws);
         const doc = connection.get("examples", id);
 
         doc.subscribe((err) => {
           if (err) throw err;
+
           const presence = connection.getPresence("examples");
           presence.subscribe((err) => {
             if (err) throw err;
           });
-          let localPresence = presence.create();
 
+          let localPresence = presence.create();
           let binding = new StringBinding(this, doc, ["content"], localPresence);
           this.setState({ binding, isLoading: false }, () =>
             console.log("binding set")
@@ -284,6 +472,7 @@ class Editor extends Component {
             let isPos =
               range.startLineNumber === range.endLineNumber &&
               range.startColumn === range.endColumn;
+
             binding.decorations = this.state.editor.deltaDecorations(
               binding.decorations,
               [
@@ -305,7 +494,7 @@ class Editor extends Component {
         });
       })
       .catch((err) => {
-        console.log(err);
+        console.error("Error fetching document:", err);
         notification.error({ message: err.toString() });
       });
   }
@@ -345,7 +534,7 @@ class Editor extends Component {
 
     axios
       .post(`${serverURL}/code/run`, {
-        code: code,
+        code,
         input: this.state.input,
         id: this.props.match.params.id,
         lang: this.state.editor.getModel().getLanguageIdentifier().language,
@@ -357,11 +546,11 @@ class Editor extends Component {
       .catch((err) => {
         if (!err.response) {
           notification.error({ message: err.toString() });
-          this.setState({ runCodeDisabled: false });
         } else if (err.response.status === 400) {
           this.state.binding._inoutListener(this.state.output, err.response.data, "output");
-          this.setState({ output: err.response.data, runCodeDisabled: false });
+          this.setState({ output: err.response.data });
         }
+        this.setState({ runCodeDisabled: false });
       });
   };
 
